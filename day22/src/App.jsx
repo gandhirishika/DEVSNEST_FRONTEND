36892d2a3f7e700922b5248c573545fc02395308
rishika <gandhirishika7@gmail.com>
2021-08-16 10:51:48 +0530

    memegenerator

---
+++ day22/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState , useEffect } from "react";
+import Templates  from "./components/Tempates";
+import './App.css';
+import Meme from "./components/Meme";
+function App() {
+const [templates , setTemplates] = useState([]);
+const [meme , setMeme] = useState(null);
+useEffect(() => {
+  fetch("https://api.imgflip.com/get_memes")
+  .then(res => res.json())
+  .then((data) => {
+    setTemplates(data.data.memes);
+  })
+} , []);
+  return (
+    <div className="App">
+<h1>Meme Generator</h1>
+{meme===null ? (<Templates templates = {templates} setMeme = {setMeme}/>)  : (<Meme meme ={meme} setMeme = {setMeme}/>)}
+
+    </div>
+  );
+}
+
+export default App;
